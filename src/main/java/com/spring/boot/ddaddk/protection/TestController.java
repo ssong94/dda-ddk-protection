b69389458749161b41ead41c07041c13caa91914
ssong94 <kyugwang@gmail.com>
2024-04-06 02:08:11 +0900

    move: 위치 변경

--- src/main/java/com/spring/boot/ddaddk/protection/TestController.java
+++ src/main/java/com/spring/boot/ddaddk/protection/TestController.java
@@ -1,4 +1,4 @@
-package com.spring.boot.ddaddk.protection.exception;
+package com.spring.boot.ddaddk.protection;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
